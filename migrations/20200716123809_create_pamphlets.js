f27ebcb947787ce1805036f3c7e4bcd2fd457a80
Wilson <wilsonj806@gmail.com>
2020-08-04 16:25:47 -0400

    Add Knex migration and seed files
    Small adjust to seed file
    
    Slight update to migrations
    
    Update Knex migration so the links array is a JSON array of links with names
    
    Delete unnecessary migration file

---
+++ migrations/20200716123809_create_pamphlets.js
@@ -0,0 +1,14 @@
+exports.up = function (knex) {
+  // NOTE: It's better practice to generate a new migration file rather than update an old one
+  return knex.schema.createTable("pamphlets", function (table) {
+    table.increments("id");
+    table.string("pamphlet_slug", 255).notNullable();
+    table.string("user", 255).defaultTo("ano");
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.json("links_array").notNullable();
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("pamphlets");
+};
---
+++ seeds/seed_pamphlets.js
@@ -0,0 +1,42 @@
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex("pamphlets")
+    .del()
+    .then(function () {
+      // Inserts seed entries
+      return knex("pamphlets").insert([
+        {
+          id: 1,
+          pamphlet_slug: "nicequirkypig",
+          created_at: knex.fn.now(),
+          links_array: JSON.stringify([
+            { link: "https://devhints.io/knex#connect-1", name: "baby shark" },
+            { link: "https://devhints.io/knex#connect-1", name: "baby shark" },
+            { link: "https://devhints.io/knex#connect-1", name: "baby shark" },
+          ]),
+        },
+        {
+          id: 2,
+          pamphlet_slug: "annoyedgroggydolphin",
+          created_at: "2020-01-01T00:00:00-05:00",
+          links_array: JSON.stringify([
+            { link: "https://devhints.io/knex#connect-1", name: "baby shark" },
+            { link: "https://devhints.io/knex#connect-1", name: "baby shark" },
+            { link: "https://devhints.io/knex#connect-1", name: "baby shark" },
+            { link: "https://devhints.io/knex#connect-1", name: "baby shark" },
+          ]),
+        },
+        {
+          id: 3,
+          pamphlet_slug: "angryhyperant",
+          created_at: knex.fn.now(),
+          links_array: JSON.stringify([
+            { link: "https://devhints.io/knex#connect-1", name: "baby shark" },
+            { link: "https://devhints.io/knex#connect-1", name: "baby shark" },
+            { link: "https://devhints.io/knex#connect-1", name: "baby shark" },
+            { link: "https://devhints.io/knex#connect-1", name: "baby shark" },
+          ]),
+        },
+      ]);
+    });
+};
