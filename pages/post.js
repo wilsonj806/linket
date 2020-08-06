60a94a4a07b4c522db29de9590437a15a8369b84
Wilson <wilsonj806@gmail.com>
2020-08-05 20:51:03 -0400

    Add crude display for the generated pamphlet

--- pages/post.js
+++ pages/post.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Input from "../components/DynFormInput";
 import usePamphlet from "../hooks/usePamphletReducer";
+import Link from "next/link";
 
 // This is a dynamic form, we're adding and removing inputs
 
@@ -9,6 +10,7 @@ const PostNewPamphlet = () => {
     links,
     loading,
     errors,
+    pamphlet_link,
     addLink,
     updateEntry,
     submitPamphlet,
@@ -32,6 +34,14 @@ const PostNewPamphlet = () => {
   };
   return (
     <section>
+      {pamphlet_link !== "" ? (
+        <div>
+          <h2>Here's the link to the new pamphlet</h2>
+          <Link href={pamphlet_link}>
+            <a>My pamphlet link</a>
+          </Link>{" "}
+        </div>
+      ) : null}
       <form>
         <h1>Create a new pamphlet</h1>
         <button type="button" className="btn btn-add-new" onClick={addLink}>
