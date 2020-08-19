74b5524ffe655b0d9f3498b3bc12fe4de1590b7c
Wilson <wilsonj806@gmail.com>
2020-08-19 12:14:15 -0400

    Update database migration to include a pamphlet name field

--- migrations/20200819115601_table_pamphlets.js
+++ migrations/20200819115601_table_pamphlets.js
@@ -1,12 +1,19 @@
 exports.up = function (knex) {
-  // NOTE: It's better practice to generate a new migration file rather than update an old one
+  // const data = await knex.select().from("pamphlets");
+  // knex.schema.dropTable("pamphlets");
   return knex.schema.createTable("pamphlets", function (table) {
     table.increments("id");
+    table.string("pamphlet_name", 255).notNullable();
     table.string("pamphlet_slug", 255).notNullable();
     table.string("user", 255).defaultTo("ano");
     table.timestamp("created_at").defaultTo(knex.fn.now());
     table.json("links_array").notNullable();
   });
+  // const newData = data.map(entry => {
+  //   return {...entry, links_array: JSON.stringify(entry.links_array)}
+  // })
+  // return knex("pamphlets").insert(newData)
+  // NOTE: It's better practice to generate a new migration file rather than update an old one
 };
 
 exports.down = function (knex) {
